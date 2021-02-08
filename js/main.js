f626c2db9ddfaaa7be96691e1983b1e0a7cc9daf
Marina.Mur <m.creation@inbox.ru>
2021-02-09 00:25:49 +0300

    update homework v3

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ function randNumber(from, to) {
   }
   return Math.floor(Math.random() * (Math.floor(to) - Math.floor(from) + 1)) + Math.floor(from);
 }
-let resultFinal = 0;
 
 // Кексобукинг
 function randNumberToFixed(from, to, quantity) {
@@ -38,24 +37,3 @@ function randNumberToFixed(from, to, quantity) {
 
   return Number(result.toFixed(quantity));
 }
-let resultFinalToFixed = 0;
-try {
-  resultFinal = randNumber(2.6, 5);
-  resultFinalToFixed = randNumberToFixed(3.6, '6.7', 10);
-}
-catch (error) {
-  console.error('Что-то пошло не так.', error);
-}
-console.log('ДЗ второе:', resultFinal);
-console.log('ДЗ второе - Кексобукинг:', resultFinalToFixed);
-// console.log('ДЗ второе:', randNumber(10, 17));
-// console.log('ДЗ второе:', randNumber(2, 1));
-// console.log('ДЗ второе:', randNumber(-1, 7));
-// console.log('ДЗ второе:', randNumber(0, 3));
-// console.log('ДЗ второе:', randNumber(4, 4));
-// console.log('ДЗ второе:', randNumber(-4, -2));
-// console.log('ДЗ второе - Кексобукинг:', randNumberToFixed(2.12212, 1.45542, 3));
-// console.log('ДЗ второе - Кексобукинг:', randNumberToFixed(-1.74638274, 7.313432, 5));
-// console.log('ДЗ второе - Кексобукинг:', randNumberToFixed(0.4432, 3.112, 1));
-// console.log('ДЗ второе - Кексобукинг:', randNumberToFixed(3.344, 4.4212, 0));
-// console.log('ДЗ второе - Кексобукинг:', randNumberToFixed(-4.3323, -2.1111, 2));
