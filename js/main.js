4173b029a952b8e49c32d1b3583330a46977da57
Marina.Mur <m.creation@inbox.ru>
2021-02-09 00:19:14 +0300

    update homework v2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 function randNumber(from, to) {
   // Проверка на тип данных: вывести ошибку, если это не число
   if (typeof from !== 'number' || typeof to !== 'number') {
-      throw new Error('Ошибка! Введите число.')
+    throw new Error('Ошибка! Введите число.')
   }
   // если первый параметр меньше нуля, то преобразовать в положительное число
   if (from < 0) {
-      from *= -1;
+    from *= -1;
   }
   // если второй параметр меньше нуля, то преобразовать в положительное число
   if (to < 0) {
-      to *= -1;
+    to *= -1;
   }
   if (from >= to) {
-      return from > to ?
-          'Значение from ' + from + ' не должно быть больше to ' + to
-          : 'Значение from ' + from + ' не должно быть равно to ' + to
-
+    return from > to ?
+      'Значение from ' + from + ' не должно быть больше to ' + to
+      : 'Значение from ' + from + ' не должно быть равно to ' + to
   }
   return Math.floor(Math.random() * (Math.floor(to) - Math.floor(from) + 1)) + Math.floor(from);
 }
@@ -24,16 +23,16 @@ let resultFinal = 0;
 // Кексобукинг
 function randNumberToFixed(from, to, quantity) {
   if (typeof from !== 'number' || typeof to !== 'number') {
-      throw new Error('Ошибка! Введите число.')
+    throw new Error('Ошибка! Введите число.')
   }
   if (from < 0 || to < 0) {
-      throw new Error('Ошибка! Некорректный диапазон.')
+    throw new Error('Ошибка! Некорректный диапазон.')
   }
 
   if (from >= to) {
-      return from > to ?
-          'Значение from ' + from + ' не должно быть больше to ' + to
-          : 'Значение from ' + from + ' не должно быть равно to ' + to
+    return from > to ?
+      'Значение from ' + from + ' не должно быть больше to ' + to
+      : 'Значение from ' + from + ' не должно быть равно to ' + to
   }
   let result = Math.random() * (to - from) + from;
 
@@ -47,8 +46,8 @@ try {
 catch (error) {
   console.error('Что-то пошло не так.', error);
 }
-console.log('ДЗ второе:', randNumber(2.6, 5));
-console.log('ДЗ второе - Кексобукинг:', randNumberToFixed(3.6, 6.7, 10));
+console.log('ДЗ второе:', resultFinal);
+console.log('ДЗ второе - Кексобукинг:', resultFinalToFixed);
 // console.log('ДЗ второе:', randNumber(10, 17));
 // console.log('ДЗ второе:', randNumber(2, 1));
 // console.log('ДЗ второе:', randNumber(-1, 7));
